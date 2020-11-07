95a585eda72a57c282ad4b4ee6beaec0b5f81fa1
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-07 11:39:49 -0300

    Using Rubocop to format the project files.

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :product do
     sequence(:name) { |n| "Product #{n}" }
     description { Faker::Lorem.paragraph }
-    price { Faker::Commerce.price( range: 100.0..400.0 ) }
+    price { Faker::Commerce.price(range: 100.0..400.0) }
     productable { nil }
   end
 end
--- spec/factories/system_requirements.rb
+++ spec/factories/system_requirements.rb
@@ -2,9 +2,9 @@ FactoryBot.define do
   factory :system_requirement do
     sequence(:name) { |n| "Basic #{n}" }
     operational_system { Faker::Computer.os }
-    storage { "50GB" }
-    processor { "AMD Ryzen 7x" }
-    memory { "4GB" }
-    video_board { "GeForce GTX3082" }
+    storage { '50GB' }
+    processor { 'AMD Ryzen 7x' }
+    memory { '4GB' }
+    video_board { 'GeForce GTX3082' }
   end
 end
