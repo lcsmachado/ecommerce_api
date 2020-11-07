63c518f45d313331f6809b0bcb1fd6942a3da4d5
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-07 19:16:13 -0300

    Creating Coupon Model, validating and testing it using Rspec

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_07_212110) do
+ActiveRecord::Schema.define(version: 2020_11_07_220153) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "coupons", force: :cascade do |t|
+    t.string "name"
+    t.string "code"
+    t.integer "status"
+    t.decimal "discount_value", precision: 5, scale: 2
+    t.integer "max_use"
+    t.datetime "due_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
---
+++ spec/factories/coupons.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :coupon do
+    name { "MyString" }
+    code { "MyString" }
+    status { 1 }
+    discount_value { "9.99" }
+    max_use { 1 }
+    due_date { "2020-11-07 19:01:53" }
+  end
+end
