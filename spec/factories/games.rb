debd4b6d6bc0947aec5fbebc3ba711fc842fa96d
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-07 17:25:26 -0300

    Using Products Factory to create Games during Tests, and changing Test Product subject to Product Factory

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :game do
     mode { %i[pvp pve both].sample }
     release_date { '2020-11-07 17:01:52' }
-    developer { Fake::Company.name }
+    developer { Faker::Company.name }
     system_requirement
   end
 end
