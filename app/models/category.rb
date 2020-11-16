e4e3538093813de57ea95cd52cf0cb57d2727904
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-16 17:37:01 -0300

    Creating name_searchable_concern[

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  include NameSearchable
+
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
   validates :name, presence: true, uniqueness: { case_sensitive: false }
