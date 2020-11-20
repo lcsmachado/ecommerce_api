7e68f5f0e87cd45a32d5373a3d5b4db1727edc5a
lcsmachado <lucascarvalhouibai@gmail.com>
2020-11-20 16:23:01 -0300

    Creating Coupons Controller and Tests with RSpec

---
+++ app/controllers/admin/v1/coupons_controller.rb
@@ -0,0 +1,45 @@
+module Admin::V1
+  class CouponsController < ApiController
+    before_action :load_coupon, only: %i[update destroy]
+
+    def index
+      @coupons = Coupon.all
+    end
+    
+    def create
+      @coupon = Coupon.new
+      @coupon.attributes = coupon_params
+      save_coupon!
+    end
+
+    def update
+      @coupon.attributes = coupon_params
+      save_coupon!
+    end
+    
+    def destroy
+      @coupon.destroy!
+    rescue StandardError
+      render_error(fields: @coupon.errors.messages)
+    end
+    
+    private
+
+    def load_coupon
+      @coupon = Coupon.find(params[:id])
+    end
+
+    def coupon_params
+      return {} unless params.has_key?(:coupon)
+
+      params.require(:coupon).permit(:code, :status, :discount_value, :due_date)
+    end
+    
+    def save_coupon!
+      @coupon.save!
+      render :show
+    rescue StandardError
+      render_error(fields: @coupon.errors.messages)
+    end
+  end
+end
---
+++ spec/requests/admin/v1/coupons/unauthenticated_requests_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe 'Admin::V1::Coupons without authentication', type: :request do
+  let(:user) { create(:user, profile: :client) }
+
+  context 'GET /admin/v1/coupons' do
+    let(:url) { '/admin/v1/coupons' }
+    let(:coupons) { create_list(:coupon, 5) }
+    before(:each) { get url }
+    include_examples 'unauthenticated access'
+  end
+  
+  context 'POST /admin/v1/coupons' do
+    let(:url) { '/admin/v1/coupons' }
+    before(:each) { post url }
+    include_examples 'unauthenticated access'
+  end
+  
+  context 'PATCH /admin/v1/coupons/:id' do
+    let(:coupon) { create(:coupon) }
+    let(:url) { "/admin/v1/coupons/#{coupon.id}" }
+    before(:each) { patch url }
+    include_examples 'unauthenticated access'
+  end
+  
+  context 'DELETE /admin/v1/coupons/:id' do
+    let(:coupon) { create(:coupon) }
+    let(:url) { "/admin/v1/coupons/#{coupon.id}" }
+    before(:each) { delete url }
+    include_examples 'unauthenticated access'
+  end
+end
