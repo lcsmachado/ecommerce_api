725b3ca3c8a51da067ec3ec5f26c5072279897b4
lcsmachado <lucascarvalhouibai@gmail.com>
2020-12-01 07:16:52 -0300

    Using Rubocop to standardize the project

--- spec/requests/auth/v1/sing_out_spec.rb
+++ spec/requests/auth/v1/sing_out_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Auth V1 Sign out", type: :request do
+RSpec.describe 'Auth V1 Sign out', type: :request do
   let(:url) { '/auth/v1/user/sign_out' }
   let!(:user) { create(:user) }
 
-  it "removes a token from User" do
+  it 'removes a token from User' do
     user_headers = auth_header(user)
     user_token = user_headers['client']
     delete url, headers: user_headers
@@ -12,7 +12,7 @@ RSpec.describe "Auth V1 Sign out", type: :request do
     expect(user.tokens.keys).to_not include(user_token)
   end
 
-  it "returns :ok status" do
+  it 'returns :ok status' do
     delete url, headers: auth_header(user)
     expect(response).to have_http_status(:ok)
   end
--- spec/requests/auth/v1/sing_up_spec.rb
+++ spec/requests/auth/v1/sing_up_spec.rb
@@ -1,38 +1,38 @@
 require 'rails_helper'
 
-RSpec.describe "Auth V1 Sign up", type: :request do
+RSpec.describe 'Auth V1 Sign up', type: :request do
   let(:url) { '/auth/v1/user' }
 
-  context "with valid params" do
+  context 'with valid params' do
     let(:user_params) { attributes_for(:user) }
 
-    it "adds new User" do
+    it 'adds new User' do
       expect {
         post url, params: user_params
       }.to change(User, :count).by(1)
     end
 
-    it "add User as :client" do
+    it 'add User as :client' do
       post url, params: user_params
       expect(User.last.profile).to eq 'client'
     end
 
-    it "returns :ok status" do
+    it 'returns :ok status' do
       post url, params: user_params
       expect(response).to have_http_status(:ok)
     end
   end
 
-  context "with invalid params" do
+  context 'with invalid params' do
     let(:user_invalid_params) { attributes_for(:user, email: nil) }
 
-    it "does not add a new User" do
+    it 'does not add a new User' do
       expect {
         post url, params: user_invalid_params
       }.to_not change(User, :count)
     end
 
-    it "return :unprocessable_entity status" do
+    it 'return :unprocessable_entity status' do
       post url, params: user_invalid_params
       expect(response).to have_http_status(:unprocessable_entity)
     end
